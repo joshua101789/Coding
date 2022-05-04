f939005daced3b3521dabb7428e477541a163c6a
joshua101789 <81497595+joshua101789@users.noreply.github.com>
2022-05-04 14:34:39 -0400

    first commit
    First commit of discount calculator for sales.

---
+++ Discount.py
@@ -0,0 +1,9 @@
+
+price = int(input("What is current price? "))
+discount = int(input("What is the discont? "))
+
+salesAmmount = ( 100- discount)/100
+
+sale = (price * salesAmmount)
+
+print(sale)
