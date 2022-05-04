3a619362806300a83bc1f9f77d0889b95b338b63
joshua101789 <81497595+joshua101789@users.noreply.github.com>
2022-05-04 14:33:44 -0400

    first commit
    First commit of working random number generator with guess tracking.

---
+++ randomGame.py
@@ -0,0 +1,27 @@
+import random
+
+x = random.randint(1, 100)
+guesses = 0
+
+print("I'm thinking of a number between 1-100... Try to guess it!!")
+while True:
+    try:
+        guess=int(input("Guess: "))
+        if guess ==x  and guesses == 0:
+            print("CHEATER!!!! YOU GOT IT RIGHT ON THE FIRST GUESS!!")
+            break
+        elif guess == x and guesses != 0:
+            print(f"Congratualtions your got it!!!! It only took {guesses} attempts!!")
+            break
+        elif guess >= x:
+            print("Not quite the number is smaller.")
+            guesses += 1
+        elif guess <= x:
+            print("Not quite the number is larger.")
+            guesses += 1
+        else:
+            print("Try a number.....")
+            guesses += 1
+    except ValueError:
+        print("Try guessing a number....")
+        guesses += 1
